43bbf9d6c1932402f35179ec21373db98b01ae6d
Stefan Buchberger <stefanbuchberger@outlook.com>
2023-11-22 01:33:44 +0100

    Added LSP

--- nvim/.config/nvim/lua/plugins/lazy.lua
+++ nvim/.config/nvim/lua/plugins/lazy.lua
@@ -94,4 +94,22 @@ require("lazy").setup({
       'nvim-treesitter/nvim-treesitter-textobjects',
     }
   },
+ -- LSP Configuration & Plugins
+  {
+    'neovim/nvim-lspconfig',
+    dependencies = {
+      -- Automatically install LSPs to stdpath for neovim
+      'williamboman/mason.nvim',
+      'williamboman/mason-lspconfig.nvim',
+
+      -- Useful status updates for LSP
+      'j-hui/fidget.nvim',
+    }
+  },
+
+ -- Autocompletion
+  {
+    'hrsh7th/nvim-cmp',
+    dependencies = { 'hrsh7th/cmp-nvim-lsp', 'L3MON4D3/LuaSnip', 'saadparwaiz1/cmp_luasnip' },
+  },
 })
