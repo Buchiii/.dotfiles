59329c5739f68a4ae6a8483b7aabd497adea2879
Stefan Buchberger <stefanbuchberger@outlook.com>
2023-12-12 23:51:05 +0100

    Add image.nvim

--- nvim/.config/nvim/lua/plugins/lazy.lua
+++ nvim/.config/nvim/lua/plugins/lazy.lua
@@ -351,46 +351,12 @@ require("lazy").setup({
   },
 
   -- image preview directly in a file
-  --{
-  --'https://github.com/3rd/image.nvim',
-  --event = "VeryLazy",
-  --  dependencies = {
-  --    {
-  --      "nvim-treesitter/nvim-treesitter",
-  --      build = ":TSUpdate",
-  --      config = function()
-  --        require("nvim-treesitter.configs").setup({
-  --          ensure_installed = { "markdown" },
-  --          highlight = { enable = true },
-  --        })
-  --      end,
-  --    },
-  --  },
-  --  opts = {
-  --    backend = "kitty",
-  --    integrations = {
-  --      markdown = {
-  --        enabled = true,
-  --        clear_in_insert_mode = false,
-  --        download_remote_images = true,
-  --        only_render_image_at_cursor = false,
-  --        filetypes = { "markdown", "vimwiki" }, -- markdown extensions (ie. quarto) can go here
-  --      },
-  --      neorg = {
-  --        enabled = true,
-  --        clear_in_insert_mode = false,
-  --        download_remote_images = true,
-  --        only_render_image_at_cursor = false,
-  --        filetypes = { "norg" },
-  --      },
-  --    },
-  --    max_width = nil,
-  --    max_height = nil,
-  --    max_width_window_percentage = nil,
-  --    max_height_window_percentage = 50,
-  --    kitty_method = "normal",
-  --  },
-  --},
+  -- works only in Kitty / Wezterm
+  -- sudo luarocks install magick --lua-version 5.1
+  -- copy /root/.luarocks to user folder and chown to user permissions
+  {
+  'https://github.com/3rd/image.nvim',
+  },
   
   -- ollama integration in nvim
   'David-Kunz/gen.nvim',
