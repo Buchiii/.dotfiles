8088dfa12606fcf3c93b65f415c872788272db5c
Stefan Buchberger <stefanbuchberger@outlook.com>
2024-01-16 23:40:12 +0100

    Update obsidian config

--- nvim/.config/nvim/lua/plugins/lazy.lua
+++ nvim/.config/nvim/lua/plugins/lazy.lua
@@ -521,34 +521,28 @@ require("lazy").setup({
 		end,
 	},
 
-	-- obsidian.md
+	-- Obsidian.md
 	{
 		"epwalsh/obsidian.nvim",
-		version = "*", -- recommended, use latest release instead of latest commit
-		lazy = true,
-		ft = "markdown",
-		-- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
-		-- event = {
-		--   -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
-		--   -- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/**.md"
-		--   "BufReadPre path/to/my-vault/**.md",
-		--   "BufNewFile path/to/my-vault/**.md",
-		-- },
-		dependencies = {
+		tag = "*", -- recommended, use latest release instead of latest commit
+		requires = {
 			-- Required.
 			"nvim-lua/plenary.nvim",
 
 			-- see below for full list of optional dependencies 👇
 		},
-		opts = {
-			workspaces = {
-				{
-					name = "personal",
-					path = "~/Documents/obsidian",
+		config = function()
+			require("obsidian").setup({
+				workspaces = {
+					{
+						name = "personal",
+						path = "~/Documents/obsidian",
+					},
 				},
-			},
-
-			-- see below for full list of options 👇
-		},
+				ui = {
+					enable = false,
+				},
+			})
+		end,
 	},
 })
