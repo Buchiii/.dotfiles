482328c8503385105c7169a7c1e15605cca175fb
Stefan Buchberger <stefanbuchberger@outlook.com>
2024-02-15 00:02:46 +0100

    Remove autosave

--- nvim/.config/nvim/lua/plugins/lazy.lua
+++ nvim/.config/nvim/lua/plugins/lazy.lua
@@ -478,15 +478,4 @@ require("lazy").setup({
 			})
 		end,
 	},
-
-	-- Autosaving
-	{
-		"Pocco81/auto-save.nvim",
-		config = function()
-			require("auto-save").setup({
-				-- your config goes here
-				-- or just leave it empty :)
-			})
-		end,
-	},
 })
