fc7c99b6bf487e8211c5ea95f25d0640dcdcc636
Stefan Buchberger <stefanbuchberger@outlook.com>
2024-06-02 16:04:42 +0200

    Remove old lint keymap

--- nvim/.config/nvim/lua/buchiii/core/keymaps.lua
+++ nvim/.config/nvim/lua/buchiii/core/keymaps.lua
@@ -3,7 +3,3 @@ vim.keymap.set("n", "<c-k>", ":wincmd k<CR>")
 vim.keymap.set("n", "<c-j>", ":wincmd j<CR>")
 vim.keymap.set("n", "<c-h>", ":wincmd h<CR>")
 vim.keymap.set("n", "<c-l>", ":wincmd l<CR>")
-
-vim.keymap.set("n", "<leader>L", function()
-	lint.try_lint()
-end, { desc = "Trigger linting for current file" })
