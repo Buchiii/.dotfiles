f31d48ac11826d0536ab67e48adaf2b8d24f8c7a
Stefan Buchberger <stefanbuchberger@outlook.com>
2024-06-02 22:45:30 +0200

    Update lualine

--- nvim/.config/nvim/lua/buchiii/plugins/lualine.lua
+++ nvim/.config/nvim/lua/buchiii/plugins/lualine.lua
@@ -1,33 +1,25 @@
 return {
-  'nvim-lualine/lualine.nvim',
-  event = 'VimEnter',
-  config = function()
-    require('lualine').setup {
-      options = {
-        icons_enabled = true,
-        component_separators = '|',
-        section_separators = '',
-      },
-      sections = {
-        lualine_x = {
-          {
-            require("noice").api.statusline.mode.get,
-            cond = require("noice").api.statusline.mode.has,
-            color = { fg = "#ff9e64" },
-          },
-          {
-            require("noice").api.status.command.get,
-            cond = require("noice").api.status.command.has,
-            color = { fg = "#ff9e64" },
-          },
-        },
-        lualine_a = {
-          {
-            'buffers',
-          }
-        }
-      }
-    }
-  end,
-  dependencies = { {'nvim-tree/nvim-web-devicons'}}
+	"nvim-lualine/lualine.nvim",
+	event = "VimEnter",
+	config = function()
+		require("lualine").setup({
+			options = {
+				icons_enabled = true,
+				component_separators = "|",
+				section_separators = "",
+			},
+			sections = {
+				lualine_x = {
+					{
+						color = { fg = "#ff9e64" },
+					},
+					{
+						color = { fg = "#ff9e64" },
+					},
+				},
+				lualine_y = { "encoding", "filetype", "progress" },
+			},
+		})
+	end,
+	dependencies = { { "nvim-tree/nvim-web-devicons" } },
 }
