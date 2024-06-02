20dfba528645320a20342ddef47b65ec549b05da
Stefan Buchberger <stefanbuchberger@outlook.com>
2024-06-02 23:02:44 +0200

    Remove toggleterm functions again

--- nvim/.config/nvim/lua/buchiii/plugins/toggleterm.lua
+++ nvim/.config/nvim/lua/buchiii/plugins/toggleterm.lua
@@ -1,16 +1,3 @@
 return {
 	{ "akinsho/toggleterm.nvim", version = "*", config = true },
-	config = function()
-		local Terminal = require("toggleterm.terminal").Terminal
-		-- btop
-		local btop = Terminal:new({ cmd = "btop", hidden = true })
-		function _BTOP_TOGGLE()
-			btop:toggle()
-		end
-		-- python
-		local python = Terminal:new({ cmd = "python", hidden = true })
-		function _PYTHON_TOGGLE()
-			python:toggle()
-		end
-	end,
 }
