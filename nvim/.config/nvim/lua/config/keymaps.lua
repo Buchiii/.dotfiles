cac0a9ec5365704abed9432eb713582805526aeb
Stefan Buchberger <stefanbuchberger@outlook.com>
2025-02-10 01:20:40 +0100

    nvim: add bufferline keymaps

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,8 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+local map = vim.keymap.set
+
+map("n", "<A-,>", "<cmd>bprevious<cr>", { desc = "Prev Buffer" })
+map("n", "<A-.>", "<cmd>bnext<cr>", { desc = "Next Buffer" })
+map("n", "<A-c>", "<cmd>bdelete<cr>", { desc = "Delete Buffer" })
